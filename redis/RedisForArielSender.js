99465e91b82e63745cad686f937e221dcd98a040
liel berniker <lielb101995@gmail.com>
2021-10-09 13:06:44 +0300

    add redis send and recive

---
+++ redis/RedisForArielSender.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = require('express')();
+var server = require('http').Server(app);
+var redis = require('redis');
+var redisClient = redis.createClient();
+var sub = redis.createClient()
+
+// for explanations : https://www.sitepoint.com/using-redis-node-js/
+
+app.get('/test', function (req, res) {
+
+    // Store string  
+    redisClient.set('counter', "12000", function (err, reply) {
+        console.log(reply);
+    });
+
+    res.send('תקשרתי עם רדיס....')
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+redisClient.on('connect', function () {
+    console.log('Sender connected to Redis');
+});
+server.listen(6062, function () {
+    console.log('Sender is running on port 6062');
+});
+

13a11aab698790fdf81f45fb933fa7bd5961d87a
tehilabk <tehilabk123@gmail.com>
2021-10-17 03:02:17 +0300

    small change, need to understand how to do the frequant item set

--- BigML/BigML.js
+++ BigML/BigML.js
@@ -2,7 +2,7 @@ var bigml = require('bigml');
 var connection = new bigml.BigML('TEHILABK123','d8379b7ff213c30f6d894078cc12f543bc9f5844')
 var source = new bigml.Source(connection);
 
-source.create('./data/iris.csv', function(error, sourceInfo) {
+source.create('./data/pack.csv', function(error, sourceInfo) {
   if (!error && sourceInfo) {
     var dataset = new bigml.Dataset();
     dataset.create(sourceInfo, function(error, datasetInfo) {
@@ -11,7 +11,7 @@ source.create('./data/iris.csv', function(error, sourceInfo) {
         model.create(datasetInfo, function (error, modelInfo) {
           if (!error && modelInfo) {
             var prediction = new bigml.Prediction();
-            prediction.create(modelInfo, {'petal length': 1})
+            prediction.create(modelInfo, {'length': 1})
           }
         });
       }

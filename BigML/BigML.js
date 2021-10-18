4280a6af6df0be47574e7ea7579a73c59b0b92df
tehilabk <tehilabk123@gmail.com>
2021-10-18 12:27:37 +0300

    add association

--- BigML/BigML.js
+++ BigML/BigML.js
@@ -18,3 +18,15 @@ source.create('../BigML/item.csv', function(error, sourceInfo) {
     });
   }
 });
+
+ // Downloads and generates a local version of the ASSOCIATION.
+ var association = new bigml.LocalAssociation(
+     'association/616d3c48e4279b24a2013597', new bigml.BigML("tehilabk123", "d8379b7ff213c30f6d894078cc12f543bc9f5844", {"domain": "bigml.io"}));
+ // To predict predict association sets fill the desired inputData
+ // inputData: input values' object.
+ var inputData = {
+         
+     };
+ association.associationSet(inputData, function (error, data) {
+     console.log(data)
+ });

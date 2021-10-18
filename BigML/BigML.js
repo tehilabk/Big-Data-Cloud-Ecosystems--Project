9bc1970c0afea9194c8427e83bb77c75fcf17586
tehilabk <tehilabk123@gmail.com>
2021-10-18 18:47:41 +0300

    fix bigml

--- BigML/BigML.js
+++ BigML/BigML.js
@@ -1,32 +1,19 @@
 var bigml = require('bigml');
-var connection = new bigml.BigML('TEHILABK123','d8379b7ff213c30f6d894078cc12f543bc9f5844')
+var connection = new bigml.BigML('tehilabk123','d8379b7ff213c30f6d894078cc12f543bc9f5844')
 var source = new bigml.Source(connection);
 
 source.create('../BigML/item.csv', function(error, sourceInfo) {
   if (!error && sourceInfo) {
-    var dataset = new bigml.Dataset();
+    var dataset = new bigml.Dataset(connection);
     dataset.create(sourceInfo, function(error, datasetInfo) {
       if (!error && datasetInfo) {
-        var model = new bigml.Model();
+        var model = new bigml.association(connection);
         model.create(datasetInfo, function (error, modelInfo) {
           if (!error && modelInfo) {
-            var prediction = new bigml.Prediction();
-            prediction.create(modelInfo, {'length': 1})
+           console.log(modelInfo)
           }
         });
       }
     });
   }
 });
-
- // Downloads and generates a local version of the ASSOCIATION.
- var association = new bigml.LocalAssociation(
-     'association/616d3c48e4279b24a2013597', new bigml.BigML("tehilabk123", "d8379b7ff213c30f6d894078cc12f543bc9f5844", {"domain": "bigml.io"}));
- // To predict predict association sets fill the desired inputData
- // inputData: input values' object.
- var inputData = {
-         
-     };
- association.associationSet(inputData, function (error, data) {
-     console.log(data)
- });

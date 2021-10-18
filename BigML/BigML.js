35441177c447d076b532ca94877b50e8f2c57fdc
tehilabk <tehilabk123@gmail.com>
2021-10-18 19:10:29 +0300

    bigml done

--- BigML/BigML.js
+++ BigML/BigML.js
@@ -8,10 +8,11 @@ source.create('../BigML/items.csv', function(error, sourceInfo) {
     var dataset = new bigml.Dataset(connection);
     dataset.create(sourceInfo, function(error, datasetInfo) {
       if (!error && datasetInfo) {
-        var model = new bigml.association(connection);
+        var model = new bigml.Association(connection);
         model.create(datasetInfo, function (error, modelInfo) {
           if (!error && modelInfo) {
            console.log(modelInfo)
+           console.log(datasetInfo)
           }
         });
       }
